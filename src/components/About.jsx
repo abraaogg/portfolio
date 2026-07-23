81ea2a55a4ad195387335c3c2dc048dc06f77147
abraaogg <abraaogoncalves1022@gmail.com>
2026-07-23 16:57:43 -0300

    about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import "../styles/about.css";
+import profileImg from '../imgs/profile.jpeg';
 
 function About() {
   return (
-    <div className='about'>About</div>
+    <div className='about'>
+      <div className='about-container'>
+        <h2>ABOUT ARIS</h2>
+        <img src={profileImg} alt='Profile Picture' />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero inventore, quas provident reprehenderit vitae voluptas, dolore ab officiis rerum, corporis cumque quae ea ratione. Corporis corrupti asperiores sapiente aliquam sunt?</p>
+      </div>
+    </div>
   )
 }
 
