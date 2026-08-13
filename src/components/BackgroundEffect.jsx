f476b5fd35f3d31c6dccd2974317030a2c156d37
abraaogg <abraaogoncalves1022@gmail.com>
2026-08-13 11:32:45 -0300

    Update BackgroundEffect.jsx

--- src/components/BackgroundEffect.jsx
+++ src/components/BackgroundEffect.jsx
@@ -7,32 +7,33 @@ gsap.registerPlugin(ScrollTrigger);
 function BackgroundEffect() {
   const circle = useRef();
 
-useLayoutEffect(() => {
-  const tween = gsap.timeline({
-    scrollTrigger: {
-      trigger: document.body,
-      start: "top top",
-      end: "bottom bottom",
-      scrub: 2,
-    },
-  });
-
-  tween
-    .to(circle.current, {
-      x: "-80vw",
-      y: "80vh",
-      ease: "none",
-    })
-    .to(circle.current, {
-      x: "0vw",
-      ease: "none",
+  useLayoutEffect(() => {
+    // "Run this code after React has created/updated the elements, but before the browser shows the updated screen to the user."
+    const tween = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.body,
+        start: "top top", // "element position" "viewport position"
+        end: "bottom bottom",
+        scrub: 2,
+      },
     });
 
-  return () => {
-    tween.kill();
-    ScrollTrigger.getAll().forEach((st) => st.kill());
-  };
-}, []);
+    tween
+      .to(circle.current, {
+        x: "-80vw",
+        y: "80vh",
+        ease: "none",
+      })
+      .to(circle.current, {
+        x: "0vw",
+        ease: "none",
+      });
+
+    return () => {
+      tween.kill();
+      ScrollTrigger.getAll().forEach((st) => st.kill());
+    };
+  }, []);
 
   return <div ref={circle} className="blur-circle" />;
 }
